1d1b5d178e9fa6c6083ad94b9cb773e7b9d1f6b7
AR <ankit.rohilla88@gmail.com>
2024-01-16 13:12:57 +1000

    JS test updates completes

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -24,12 +24,56 @@ describe('Various Automated Example', () => {
         cy.location("pathname").should("equal","/best-practices")
     })
 
-    it.only('Validate interception mechanism', () => {
+    it('Validate interception mechanism', () => {
         cy.intercept('POST','http://localhost:3000/examples', {
-            body: {
-                message: 'successfully intercepted request'
-            }
+            body: {message: 'successfully intercepted request'}
+            //fixture: 'example.json'
         })
         cy.getDataTest('post-button').click()
+    })
+
+    it.only('Validate grudges', () => {
+        cy.contains(/add some grudges/i)
+        cy.getDataTest('clear-button').should('not.exist')
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').should('have.length',0)
+        })
+
+        //add first grudge
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('my grudge #1')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').should('have.length',1)
+        })
+        
+        //add second grudge
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('my grudge #2')
+        })
+        cy.getDataTest('add-grudge-button').click()
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').should('have.length',2)
+            cy.get('li').its(0).should('contains.text','my grudge #1')
+        })
+        cy.getDataTest('clear-button').should('exist')
+
+        //remove first grudge
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').its(0).within(() => {
+                cy.get('button').click()
+            })
+        })
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').should('have.length',1)
+        })
+        
+        //use clear button to remove all grudges
+        cy.getDataTest('clear-button').click()
+        cy.getDataTest('my-grudge-list').within(() => {
+            cy.get('li').should('have.length',0)
+        })
     })
 })
